4e3376f004e915fe6b66a10509ab5cbfc16a1be1
antosha-85 <antosha-85@yandex.ru>
2020-01-16 22:32:37 +0000

    Added the very first part of cats with refactored callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,16 @@
 const request = require('request');
 const args = process.argv;
 const search = args[2];
-const api = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
 
-request(api , (error, response, body) => {
-  if (error) {
-    console.log('there was an error with the request:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-    console.log(data);
-    console.log(typeof data);
-  }
-});
+
+function fetchBreedDescription(breedName, cb) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}` , (error, response, body) => {
+        if (error) {
+            cb(err, null);
+        } else {
+            cb(null, body);
+        }
+      });
+}
+
+module.exports = { fetchBreedDescription };
