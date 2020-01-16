5c5cfb310426f698c2ff039c9ce3beb0f7c9780b
antosha-85 <antosha-85@yandex.ru>
2020-01-16 23:15:22 +0000

    Finished first part of cats Apis

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,14 @@
 const request = require('request');
-const args = process.argv;
-const search = args[2];
 
 
-function fetchBreedDescription(breedName, cb) {
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}` , (error, response, body) => {
-        if (error) {
-            cb(err, null);
-        } else {
-            cb(null, body);
-        }
-      });
-}
+const fetchBreedDescription = (breedName, cb) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}` , (error, response, body) => {
+    if (error) {
+      cb(error, null);
+    } else {
+      cb(null, body);
+    }
+  });
+};
 
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription (breedName, (error, desc) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
-      const data = JSON.parse(desc);
-      console.log(data);
+    const data = JSON.parse(desc);
+    console.log(data);
     console.log(typeof data);
   }
 });
-
-
