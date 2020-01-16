05543c6f14f7cb6bf236af22aa7e6b42c2b34ef9
antosha-85 <antosha-85@yandex.ru>
2020-01-16 20:52:33 +0000

    Finished the cat api task

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,14 @@
 const request = require('request');
+const args = process.argv;
+const search = args[2];
+const api = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
+
+request(api , (error, response, body) => {
+  if (error) {
+    console.log('there was an error with the request:', error); // Print the error if one occurred
+  } else {
+    const data = JSON.parse(body);
+    console.log(data);
+    console.log(typeof data);
+  }
+});
